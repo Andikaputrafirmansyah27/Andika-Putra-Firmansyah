c2768af9076973cb75b6877a9edf5a35efffd222
Andikaputrafirmansyah27 <151049726+Andikaputrafirmansyah27@users.noreply.github.com>
2023-11-16 22:03:11 +0700

    Add files via upload

---
+++ Identitas.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Andika
+ */
+public class Identitas {
+    public static void main (String[] args) {
+        String nama = "Andika Putra Firmansyah";
+        int umur = 19;
+        float tb = 173;
+        
+        String prodi = "Ilmu Komputer";
+        String nim = "I.2310513";
+        String kampus = "Djuanda Bogor";
+        
+        System.out.println("Assalamualaikum Wr. Wr.");
+        System.out.println("Nama saya "+ nama);
+        System.out.println("Umur saya "+ umur);
+        System.out.println("Tinggi saya" + tb);
+        
+        System.out.println("Saya dari prodi" + prodi + "dengan NIM " + nim);
+        System.out.println("Saya dari Universitas" + kampus);
+    }
+}
